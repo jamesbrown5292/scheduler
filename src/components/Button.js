68355672cf437790ccf5bb6e0ec95fbb4fcddf33
jamesbrown5292 <jamesbrown5292@gmail.com>
2020-09-28 13:13:57 -0700

    add button styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,11 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   return (
+     <button className="Button">{props.children}</button> 
+   );
 }
+
+
+//Change the component in here to render a button.
+// It should use props.children value as the button text. 
