b9c49333fffb05eb1e5d95079040e95185d28b40
jamesbrown5292 <jamesbrown5292@gmail.com>
2020-09-28 16:24:37 -0700

    add day list item and styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,30 +1,25 @@
 import React from "react";
-
 import "components/Button.scss";
 import { action } from "@storybook/addon-actions";
+import classNames from "classnames";
 
 export default function Button(props) {
-   let buttonClass = "button";
-
-   if (props.confirm) {
-      buttonClass += " button--confirm"
-   }
-
-   if (props.danger) {
-      buttonClass += " button--danger"
-   }
-
+   const buttonClass = classNames('button', {
+      'button--confirm': props.confirm,
+      'button--danger': props.danger
+   });
+   
 
 
    return (
       <button 
          className={buttonClass} 
-         onClick={action("button-clicked")}
+         onClick={props.onClick}
          disabled = {props.disabled}
       >
                         
          {props.children}
-         
+
       </button>
    );
 }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+
+export default function DayListItem(props) {
+  
+  const listItemClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0
+  })
+
+  return (
+    <li className={listItemClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">Day Name</h2> 
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
