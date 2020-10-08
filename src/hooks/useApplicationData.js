e98aeb1dfdc074f1c180501e3394bab378439e95
jamesbrown5292 <jamesbrown5292@gmail.com>
2020-10-07 19:50:16 -0700

    implement reducer logic

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,17 +1,41 @@
-import { useState, useEffect }  from "react";
+import { useState, useEffect, useReducer, dispatch }  from "react";
 import axios from "axios";
+
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+
 
 
 export default function useApplicationData(initial) {
 
-  const [state, setState] = useState({
+  const reducer = (state, action) => {
+
+    switch (action.type) {
+      case SET_DAY:
+        return {}
+      case SET_APPLICATION_DATA:
+        return {}
+      case SET_INTERVIEW: 
+        return {}
+      default:
+        throw new Error(
+          `Tried to reduce with unsupported action type ${action.type}`
+        )
+  
+    }
+  
+  }
+
+
+  const [state, setState] = useReducer({
     day: 'Monday',
     days: [],
     appointments: {},
     interviewers: {}
   });
   
-  const setDay = day => setState({ ...state, day });
+  const setDay = day => dispatch({ type: SET_DAY, value: {...state, day} });
   
   useEffect(() => {
 
